24c4a0c9914b16be15e925b0c965e94ff5d8c787
Daniel Tshuva <danielts86@gmail.com>
2018-08-31 10:44:11 +0300

    hunspell

--- WhatIsInAName.Infrastructure/SpellService.cs
+++ WhatIsInAName.Infrastructure/SpellService.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using NHunspell;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +11,31 @@ namespace WhatIsInAName.Infrastructure
 {
     public class SpellService
     {
+        public SpellService()
+        {
+            var path = Assembly.GetExecutingAssembly().Location;
+            path = Path.GetDirectoryName(path);
+            Hunspell.NativeDllPath = path;
+        }
         public void Get()
         {
+            try
+            {
+                using (Hunspell hunspell = new Hunspell("en_us.aff", "en_us.dic"))
+                {
+                    Console.WriteLine("Make suggestions for the misspelled word 'Recommendation'");
+                    List<string> suggestions = hunspell.Suggest("Recommendation");
+                    Console.WriteLine("There are " + suggestions.Count.ToString() + " suggestions");
+                    foreach (string suggestion in suggestions)
+                    {
+                        Console.WriteLine("Suggestion is: " + suggestion);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
 
+            }
         }
     }
 }
