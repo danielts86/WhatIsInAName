04dcef646fd6b39ecf2cfa936916a585d5c229f6
Daniel Tshuva <danielts86@gmail.com>
2018-08-31 08:06:24 +0300

    Add MainWindow

---
+++ WhatIsInAName/MainWindow.cs
@@ -0,0 +1,34 @@
+﻿namespace WhatIsInAName
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("f06126f4-65e6-4a55-9f38-956f37a12b40")]
+    public class MainWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow() : base(null)
+        {
+            this.Caption = "MainWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new MainWindowControl();
+        }
+    }
+}
