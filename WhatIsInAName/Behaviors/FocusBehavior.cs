6efedbbd6434fed1dc68dcb288233d4a9869b5a1
Daniel Tshuva <danielts86@gmail.com>
2018-09-02 20:59:56 +0300

    VariableWordView

---
+++ WhatIsInAName/Behaviors/FocusBehavior.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows;
+
+namespace WhatIsInAName.Behaviors
+{
+    public static class FocusBehavior
+    {
+        public static readonly DependencyProperty IsFocusedProperty =
+            DependencyProperty.RegisterAttached("IsFocused", typeof(bool?), typeof(FocusBehavior), new FrameworkPropertyMetadata(IsFocusedChanged) { BindsTwoWayByDefault = true });
+
+        public static bool? GetIsFocused(DependencyObject element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            return (bool?)element.GetValue(IsFocusedProperty);
+        }
+
+        public static void SetIsFocused(DependencyObject element, bool? value)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            element.SetValue(IsFocusedProperty, value);
+        }
+
+        private static void IsFocusedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var element = (FrameworkElement)d;
+
+            if (e.OldValue == null)
+            {
+                element.GotFocus += FrameworkElementGotFocus;
+                element.LostFocus += FrameworkElementLostFocus;
+            }
+
+            if (!element.IsVisible)
+            {
+                element.IsVisibleChanged += new DependencyPropertyChangedEventHandler(FrameworkElementIsVisibleChanged);
+            }
+
+            if ((bool)e.NewValue)
+            {
+                element.Focus();
+            }
+        }
+
+        private static void FrameworkElementIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var fe = (FrameworkElement)sender;
+            if (fe.IsVisible && (bool)((FrameworkElement)sender).GetValue(IsFocusedProperty))
+            {
+                fe.IsVisibleChanged -= FrameworkElementIsVisibleChanged;
+                fe.Focus();
+            }
+        }
+
+        private static void FrameworkElementGotFocus(object sender, RoutedEventArgs e)
+        {
+            ((FrameworkElement)sender).SetValue(IsFocusedProperty, true);
+        }
+
+        private static void FrameworkElementLostFocus(object sender, RoutedEventArgs e)
+        {
+            ((FrameworkElement)sender).SetValue(IsFocusedProperty, false);
+        }
+    }
+}
